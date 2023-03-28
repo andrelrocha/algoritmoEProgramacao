925e38c9d2c4d95a2066fa6d5de61eba7ee46abe
andrelrocha <andrerocha0911@gmail.com>
2023-03-28 15:31:01 -0300

    conversor dolar real

--- exercicios/24.03/questao7.C
+++ exercicios/24.03/questao7.C
@@ -1,2 +1,16 @@
 //Elaborar um algoritmo que apresente o valor da conversao em Real de um valor lido em Dolar. O
 //algoritmo deve receber o valor da cotacao do dolar no dia.
+
+#include <stdio.h>
+
+int main () {
+    double valorReal = 0;
+    printf("Digite o valor em reais: ");
+    scanf(" %lf", &valorReal);
+
+    double valorDolar = 5.20;
+    
+    double valorFinal = valorReal*valorDolar;
+
+    printf("Para R$ %.2lf, voce tera $ %.2lf", valorReal, valorFinal);
+}
