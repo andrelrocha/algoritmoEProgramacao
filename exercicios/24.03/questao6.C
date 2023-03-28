8eca53e43a15d4574dcae860da9f5f411407a459
andrelrocha <andrerocha0911@gmail.com>
2023-03-28 15:30:41 -0300

    calcula volume do retangulo

--- exercicios/24.03/questao6.C
+++ exercicios/24.03/questao6.C
@@ -1,1 +1,19 @@
 //Elaborar um algoritmo que apresente o valor do volume de uma caixa retangular, utilizando a formula Volume = Comprimento * Largura * Altura.
+
+#include <stdio.h>
+
+int main () {
+    float comprimento, largura, altura;
+    printf("Digite a altura: ");
+    scanf(" %f", &altura);
+
+    printf("Digite a largura: ");
+    scanf(" %f", &largura);
+
+    printf("Digite o comprimento: ");
+    scanf(" %f", &comprimento);
+
+    float volume = comprimento*largura*altura;
+
+    printf("O volume total do retangulo de altura %.2f, largura %.2f e comprimento %.2f e: %.2f", altura, largura, comprimento, volume);
+}
