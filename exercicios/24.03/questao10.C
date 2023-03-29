d429fb3d9a15bec1adfc19a82478c50dec67a9c3
andrelrocha <andrerocha0911@gmail.com>
2023-03-29 13:47:14 -0300

    finalizando os exercícios

--- exercicios/24.03/questao10.C
+++ exercicios/24.03/questao10.C
@@ -1,8 +1,13 @@
 //Escreva um algoritmo que leia uma temperatura em graus Farenheit e a apresente em graus Centigrados.
-//A convers~ao de graus Farenheit para Centigrados e obtida por C=(5/9)(F-32).
+//A conversao de graus Farenheit para Centigrados e obtida por C=(5/9)(F-32).
 
 #include <stdio.h>
 
 int main () {
-    
+    float fah = 0;
+    printf("digite a temperatura em fahrenheit:\n");
+    scanf(" %f", &fah);
+
+    float celsius = (5/9.0)*(fah-32);
+    printf("%.f graus fahrenheit e o mesmo que %.f C", fah, celsius);
 }
--- exercicios/24.03/questao8.C
+++ exercicios/24.03/questao8.C
@@ -4,7 +4,30 @@
 //• Se delta menor que zero: imprima a mensagem "Nao Existem Raizes Reais".
 
 #include <stdio.h>
+#include <math.h>
 
 int main () {
-    
+    int a, b , c = 0;
+
+    printf("sejam a, b e c pertecentes aos Inteiros e coeficientes de uma equacao de segundo grau, ");
+    printf("escreva o coeficiente a:\n");
+    scanf(" %d", &a);
+    printf("escreva o coeficiente b:\n");
+    scanf(" %d", &b);
+    printf("escreva o coeficiente c:\n");
+    scanf(" %d", &c);
+
+    int delta = (b*b) - (4 * a * c);
+
+    if (delta > 0) {
+        double resultado1 = (((-b) + sqrt(delta)) / (2 * a));
+        double resultado2 = (((-b) - sqrt(delta)) / (2 * a));
+        printf("as raizes da equacao sao %.2lf e %.2lf\n", resultado1, resultado2); 
+    } else if (delta == 0) {
+        double xVertice = (- b / (2 * a));
+        printf("a raiz da equacao e %.2lf", xVertice);
+    } else {
+        printf("nao existem raizes reais\n");
+    }
+   
 }
